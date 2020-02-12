56985b623547653b93dafc45abb680f676be0502
Phuong LÊ <phuong.le@menincode.com>
2020-02-12 15:45:16 +0000

    Work on Todo App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Todo from './Components/Todo';
+import TodoForm from './Components/TodoForm';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const initialTodo = [
+    { id: 1, task: "Learn ReactJs", completed: false },
+    { id: 2, task: "Learn VueJs", completed: false }
+  ]
+  const [todoList, updateTodoList] = useState(initialTodo);
+
+  const handleCompleted = (id) => {
+
+    let newTodoList = [...todoList];
+
+    newTodoList.map(todo => {
+      if (todo.id === id) {
+        todo.completed = true
+        return todo
+      }
+    })
+    updateTodoList(newTodoList)
+  }
+
+  const handleAdd = (todo) => {
+    let newTodoList = [...todoList]
+    newTodoList.push(todo)
+    updateTodoList(newTodoList)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="App-header">
+      <h1>MY TODO LIST</h1>
+        <div className="frame">
+          <div className="container">
+            <ul className="list-group">
+              <Todo lists={todoList} completeTodo={handleCompleted} />
+            </ul>
+            <div className="row">
+              <TodoForm addTodo={handleAdd} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/TodoForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function TodoForm({ addTodo }) {
+
+    const [value, setValue] = useState('')
+
+
+    const updateValue = (e) => {
+        setValue(e.currentTarget.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let newTodo = {
+            id: new Date().getTime(),
+            task: value,
+            completed: false
+        }
+        addTodo(newTodo)
+        setValue('')
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="row">
+                    <input className="form-control form-control-lg" type="text" onChange={updateValue}  value={value} placeholder="Add Your Todo here :)" />
+            </div>
+            <div className="row">
+                    <button className="btn btn-primary myBtn">Add Todo</button>
+            </div>
+        </form>
+    )
+}
+
+export default TodoForm;
