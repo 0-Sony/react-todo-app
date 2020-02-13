0738188363267fcbf8d3f20e3d8a0176e568c66c
Puyan Wei <puyanwei@gmail.com>
2020-02-13 21:34:43 +0000

    Linting adjustment

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Todo({ lists, completeTodo }) {
+    const handleClick = id => {
+        completeTodo(id);
+    };
+    return lists.map(function(list) {
+        return (
+            <li
+                className={
+                    "list-group-item " +
+                    (list.completed ? "completed list-group-item-success" : "")
+                }
+                key={list.id}
+            >
+                {list.task}
+                <button onClick={() => handleClick(list.id)}> X </button>
+            </li>
+        );
+    });
+}
+
+export default Todo;
